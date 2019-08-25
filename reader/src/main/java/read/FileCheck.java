467c4826ffd8d46c6f93364ee63b4b5493bc237b
Jacek <jaceksawko95@gmail.com>
2019-08-25 15:00:20 +0100

    oczyszczanie

--- reader/src/main/java/read/FileCheck.java
+++ reader/src/main/java/read/FileCheck.java
@@ -3,8 +3,7 @@ package read;
 public interface FileCheck {
 
     public static String checkFileExtension(String filePath){
-        String fileExtension = filePath.substring(filePath.lastIndexOf(".")+1);
-        return fileExtension;
+        return filePath.substring(filePath.lastIndexOf(".")+1);
     }
 
 }
--- reader/src/main/java/testing/Main.java
+++ reader/src/main/java/testing/Main.java
@@ -5,9 +5,6 @@ import database.Connect;
 public class Main {
 
     public static void main(String[] args) {
-        String asd = "C:\\Users\\Jacek\\Desktop\\test.csv";
         Connect.runReader();
-
     }
-
 }
